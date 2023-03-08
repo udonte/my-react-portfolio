58489cd5d0f1de9aa37bd8c14d83364378e098a3
George Udonte <georgeudonte@gmail.com>
2023-03-08 09:42:05 +0100

    adjusted about

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
           <article className="about__card">
             <FaAward className='about__icon' />
             <h5>Experience</h5>
-            <small>2 Years Working</small>
+            <small>1yr+ Working</small>
           </article>
 
           <article className="about__card">
